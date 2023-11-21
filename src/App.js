0aa2a1c2d326f0e2ce532b8365a8259463d438b5
Annie Zhang <anniezhang2288@berkeley.edu>
2023-11-21 10:43:25 -0800

    intial website

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
-function MyButton() {
-  return (
-    <button>
-      I'm a button
-    </button>
-  );
-}
+import logo from './logo.svg';
+import './App.css';
 
-export default function MyApp() {
+function App() {
   return (
-    <div>
-      <h1>Welcome to my app</h1>
-      <MyButton />
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          meow :D
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
+
+
+
+export default App;
